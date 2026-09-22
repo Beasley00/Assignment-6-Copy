62d8018adb7bf82ad9d16b1cb87567f374512053
Beasley00 <carter.beasley@gmail.com>
2026-09-22 11:34:16 -0500

    first commit

---
+++ Workout.java
@@ -0,0 +1,26 @@
+// Carter Beasley | Assignment 5 | Workout.java
+
+public class Workout extends TrainingSessions 
+{
+    // Attributes
+    protected String workout;
+
+    // Constructor
+    public Workout(String startTime, String date, String workout) 
+    {
+        super(startTime, date);
+        this.workout = workout;
+    }
+
+    // Getters and Setters
+    public String getWorkout() 
+    {
+        return workout;
+    }
+
+    public void setWorkout(String workout) 
+    {
+        this.workout = workout;
+    }
+}
+
